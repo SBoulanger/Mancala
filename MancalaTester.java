f09e9070cf78167e69134770136c670bb7268338
hocindyxi <cindy.x.ho@sjsu.edu>
2017-04-18 17:14:22 -0700

    Update MancalaTester.java

--- MancalaTester.java
+++ MancalaTester.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 
-
-class MancalaTester {
+public class MancalaTester {
+	public MancalaTester(){
+		
+	}
 	
-	public static void main(String[] args){
-		System.out.println("Hello Mancala");
-}
-
+	public static void main (String args[]){
+		System.out.println("Hi, Welcome to Mancala!");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("How many stones would you like to start with? 3 or 4?");
+		int amtStart = scanner.nextInt();
+		
+		if(amtStart == 3)
+			Board board = new Board(3);
+		if(amtStart == 4)
+			Board board = new Board(4);
+	}
 }
