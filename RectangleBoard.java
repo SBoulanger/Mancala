5245ed50e014c7d94c31896a9ee2786f03e231ae
Aldo Barrientos Calero <aldonut@Aldos-MacBook-Pro-2.local>
2017-04-22 17:43:06 -0700

    current

---
+++ RectangleBoard.java
@@ -0,0 +1,73 @@
+package mancala;
+
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.geom.Ellipse2D;
+
+import javax.swing.Icon;
+
+public class RectangleBoard implements Icon
+{
+	private DataModel model;
+	private Hole[] holes;
+	private int holeX = 150;
+	private int holeY = 50;
+	
+	public RectangleBoard(DataModel model)
+	{
+		this.model = model;
+		holes = model.getData();
+	}
+	
+	
+	public void paintIcon(Component c, Graphics g, int x, int y) 
+	{
+        Graphics2D g2 = (Graphics2D) g;
+
+        for(int i = 0; i < 14; i++)
+        {
+        	if(i == 7 || i == 0)
+    			continue;
+            Hole hole = new Hole(model.startPebbles, i);
+            hole.draw(holeX, holeY, g2);
+        	holes[i].drawStones(g2, holeX, holeY);
+            translateHoles(i);
+        }
+        
+        Ellipse2D mancalaZero = new Ellipse2D.Double(50, 50, 100, 200);
+        Ellipse2D mancalaSeven = new Ellipse2D.Double(1800, 50, 100, 200);
+
+
+        Rectangle board = new Rectangle(0, 0, 2000, 300);
+
+        g2.draw(board);
+        
+	}
+	
+	public void translateHoles(int pos)
+	{
+		
+		if(pos == 6)
+		{
+			holeX = 150;
+			holeY = 100;
+			return;
+		}
+		
+		holeX += 50;
+	}
+	
+	
+
+	public int getIconWidth() 
+	{
+		return 0;
+	}
+
+	public int getIconHeight() 
+	{
+		return 0;
+	}
+}
