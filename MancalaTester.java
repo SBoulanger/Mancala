cbe04674180b35eb4597c6f52f6adbf711c41e8e
aldonut <aldo.barrientoscalero@sjsu.edu>
2017-04-26 15:10:54 -0700

    added visual components to test

--- MancalaTester.java
+++ MancalaTester.java
@@ -7,12 +7,21 @@ public class MancalaTester {
 	}
 	
 	public static void main (String args[]){
-		System.out.println("Hi, Welcome to Mancala!");
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("How many stones would you like to start with? 3 or 4?");
-		int amtStart = scanner.nextInt();
-		DataModel dm = new DataModel(amtStart);
-		Board b = new Board(dm);
+                System.out.println("Hi, Welcome to Mancala!");
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("How many stones would you like to start with? 3 or 4?");
+                int amtStart = scanner.nextInt();
+                DataModel dm = new DataModel(amtStart);
+                Board b = new Board(dm);
+                RectangleBoard rectBoard = new RectangleBoard(dm);
+                
+                JLabel myLabel = new JLabel(rectBoard);
+                b.add(myLabel);
+                b.setLayout(new FlowLayout());
+                b.pack();
+                b.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                b.setVisible(true);
+
 		
 	}
 }
